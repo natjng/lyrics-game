efa6a173a92543fb9ffe6c1f69928ea7cf26306a
Natalie <natsetc@gmail.com>
2019-09-11 16:38:40 -0700

    create Game index, show, and create actions

--- lyrics-game-backend/app/controllers/games_controller.rb
+++ lyrics-game-backend/app/controllers/games_controller.rb
@@ -1,2 +1,16 @@
 class GamesController < ApplicationController
+    def index
+        games = Game.all
+        render json: games
+    end
+
+    def show
+        game = Game.find_by(id: params[:id])
+        render json: game
+    end
+
+    def create
+        game = Game.create(score: params[:score], user_id: params[:user_id])
+        render json: game
+    end
 end
