6a685f4fb3c5355b58f3ceddb96d77f38b4b6755
Natalie <natsetc@gmail.com>
2019-09-11 13:42:04 -0700

    create songs show action

--- lyrics-game-backend/app/controllers/songs_controller.rb
+++ lyrics-game-backend/app/controllers/songs_controller.rb
@@ -3,4 +3,9 @@ class SongsController < ApplicationController
         songs = Song.all
         render json: songs
     end
+
+    def show
+        song = Song.find_by(id: params[:id])
+        render json: song
+    end
 end
