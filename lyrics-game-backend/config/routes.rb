30c6efdb24ea2398740625a6e93b290b1b3f979a
Natalie <natsetc@gmail.com>
2019-09-11 13:44:31 -0700

    create Game resource

--- lyrics-game-backend/config/routes.rb
+++ lyrics-game-backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :games
+  resources :users
   resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- lyrics-game-backend/db/schema.rb
+++ lyrics-game-backend/db/schema.rb
@@ -10,11 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_191442) do
+ActiveRecord::Schema.define(version: 2019_09_11_203042) do
+
+  create_table "games", force: :cascade do |t|
+    t.integer "score"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "name"
     t.string "lyrics"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
