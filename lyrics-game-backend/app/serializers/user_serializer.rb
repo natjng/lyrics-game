bdef3aa9773ccce2ec648a52d6341dc0ffa27208
Natalie <natsetc@gmail.com>
2019-09-14 20:21:05 -0700

    create custom attribute for user serializer

--- lyrics-game-backend/app/serializers/user_serializer.rb
+++ lyrics-game-backend/app/serializers/user_serializer.rb
@@ -2,4 +2,8 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :username
   has_many :games
+
+  attributes :highest_score do |object|
+    "#{object.games.map(&:score).max}"
+  end
 end
