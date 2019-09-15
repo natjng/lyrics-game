159ff2fd1d6abb68341671394740b0e515682f51
Natalie <natsetc@gmail.com>
2019-09-14 20:20:31 -0700

    use strong params for User create action

--- lyrics-game-backend/app/controllers/users_controller.rb
+++ lyrics-game-backend/app/controllers/users_controller.rb
@@ -8,10 +8,16 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.find_or_create_by(username: params[:username])
+        user = User.find_or_create_by(user_params)
         options = {
             include: [:games]
         }
         render json: UserSerializer.new(user, options)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
     end
 end
