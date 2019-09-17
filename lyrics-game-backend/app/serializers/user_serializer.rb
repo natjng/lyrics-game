0be7fb9ea88adc44ff556e75ab4b7c10dd4d586f
Natalie <natsetc@gmail.com>
2019-09-17 10:35:15 -0700

    create custom attr for high score game

--- lyrics-game-backend/app/serializers/user_serializer.rb
+++ lyrics-game-backend/app/serializers/user_serializer.rb
@@ -3,7 +3,12 @@ class UserSerializer
   attributes :username
   has_many :games
 
-  attributes :highest_score do |object|
+  attributes :high_score do |object|
     "#{object.games.map(&:score).max}"
+  end
+
+  attributes :high_score_game do |object|
+    high_score = object.games.map(&:score).max
+    object.games.detect{|g| g.score == high_score}
   end
 end
